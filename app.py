962ee500cd902a4e2a5132aa237b06bdedbffbfe
Mateus <mateusmferreira@live.com>
2019-02-22 14:25:18 -0300

    Projeto com gráfico

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import database
 import mysql.connector as mariadb
+import matplotlib.pyplot
+import tkinter
 
+# Define os parametros
 mariadb_connection = database.mariadb_connection()
-
 cursor = mariadb_connection.cursor()
+firstname_list = []
+age_list = []
 
-cursor.execute("SELECT firstname, lastname, age FROM customer")
+# Efetua o select dos dados
+cursor.execute("SELECT firstname, age FROM customer")
 
-for first_name, last_name, age in cursor:
-    print("First name: {}, Last name: {}, Age: {}".format(first_name,last_name, age))
+# Cria as listas com os nomes e a idade
+for firstname, age in cursor:
+    firstname_list.append(firstname)
+    age_list.append(age)
+
+matplotlib.pyplot.plot(firstname_list, age_list)
+matplotlib.pyplot.show()
