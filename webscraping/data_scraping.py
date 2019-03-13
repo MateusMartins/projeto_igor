ecc0ed8f383b00b45b965e0b777575f7aeed5605
Mateus <mateusmferreira@live.com>
2019-03-12 21:01:07 -0300

    adicao do filtro regex

--- webscraping/data_scraping.py
+++ webscraping/data_scraping.py
@@ -1,4 +1,5 @@
 from config import *
+import re
 
 def get_header(driver, header = []):
         for _ in range(5):
@@ -11,9 +12,9 @@ def get_value(driver, header = [], data_dict = {}):
                 i = 1
                 for key in header:
                         if key in data_dict.keys():
-                                data_dict[key].append(driver.find_element_by_xpath('/html/body/div/div[3]/div[2]/form/div[5]/div/div[6]/table/tbody/tr[{}]/td[{}]'.format(_ + 2, i)).text)
+                                data_dict[key].append(check_value(driver, driver.find_element_by_xpath('/html/body/div/div[3]/div[2]/form/div[5]/div/div[6]/table/tbody/tr[{}]/td[{}]'.format(_ + 2, i)).text))
                         else:
-                                data_dict[key] = [driver.find_element_by_xpath('/html/body/div/div[3]/div[2]/form/div[5]/div/div[6]/table/tbody/tr[{}]/td[{}]'.format(_ + 2, i)).text]
+                                data_dict[key] = [check_value(driver, driver.find_element_by_xpath('/html/body/div/div[3]/div[2]/form/div[5]/div/div[6]/table/tbody/tr[{}]/td[{}]'.format(_ + 2, i)).text)]
                         i += 1
         return data_dict
 
@@ -22,3 +23,9 @@ def get_botao_avancar(driver):
 
 def get_table(driver):
         return driver.find_element_by_xpath('//*[@id="ctl00_cphConteudo_gdvResultadoBusca_gdvContent"]')
+
+def check_value(driver, value):
+        if re.findall('\'|\"', value):
+                return re.sub('\'|\"', '-', value)
+        else:
+                return value
